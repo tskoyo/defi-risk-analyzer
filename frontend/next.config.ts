5cef56f4f492f1fc0f2e8f6bcc31df5d819bed30
Spyro7883 <spinu11_2003@yahoo.ro>
2026-03-24 20:40:52 +0200

    basePath added to next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/defi-risk-analyzer",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
